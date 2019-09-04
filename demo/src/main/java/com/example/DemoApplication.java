1ef6763e87c10e572ff6541c28bbb86b3f14c3a7
HenryGe <genffe@hotmail.com>
2019-09-04 17:17:23 +0800

    2091-09-04提交

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,13 +1,11 @@
-package com.example.demo;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
 
-@EnableEurekaClient
+//@EnableEurekaClient
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.*"})
+//@ComponentScan(basePackages = {"com.example.*"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- ribbon-consumer/src/main/java/com/henry/ribbonconsumer/HelloService.java
+++ ribbon-consumer/src/main/java/com/henry/ribbonconsumer/HelloService.java
@@ -2,9 +2,11 @@ package com.henry.ribbonconsumer;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration
 @Service
 public class HelloService {
 
